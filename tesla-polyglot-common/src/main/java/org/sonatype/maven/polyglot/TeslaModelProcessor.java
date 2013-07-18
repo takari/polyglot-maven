bfc16c7c6eb4e5660780c5e2f4e3e777ad8b1589
Christian Meier <m.kristian@web.de>
2013-07-18 20:10:12 +0100

    dump xml after parsing model so install and deploy work with pom.xml
    allow to specify a dump filename and whether it is readonly via properties of the model

--- tesla-polyglot-common/src/main/java/org/sonatype/maven/polyglot/TeslaModelProcessor.java
+++ tesla-polyglot-common/src/main/java/org/sonatype/maven/polyglot/TeslaModelProcessor.java
@@ -23,21 +23,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.StringWriter;
 import java.util.Map;
 
 import org.apache.maven.model.Model;
+import org.apache.maven.model.building.FileModelSource;
 import org.apache.maven.model.building.ModelProcessor;
+import org.apache.maven.model.building.ModelSource;
 import org.apache.maven.model.io.ModelParseException;
 import org.apache.maven.model.io.ModelReader;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.IOUtil;
 
 /**
  * Polyglot model processor.
- * 
+ *
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
- * 
+ *
  * @since 0.7
  */
 @Component(role = ModelProcessor.class, hint = "tesla-polyglot")
@@ -49,6 +54,18 @@ public class TeslaModelProcessor implements ModelProcessor {
   public File locatePom(final File dir) {
     assert manager != null;
     File pomFile = manager.locatePom(dir);
+    if ( pomFile != null ){
+        pomFile = new File( pomFile.getParentFile(), ".tesla." + pomFile.getName() );
+        try
+        {
+            pomFile.createNewFile();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException( "error creating empty file", e );
+        }
+    }
+
     // behave like proper maven in case there is no pom from manager
     return pomFile == null ? new File(dir, "pom.xml") : pomFile;
   }
@@ -70,10 +87,46 @@ public class TeslaModelProcessor implements ModelProcessor {
     return read(new InputStreamReader(input), options);
   }
 
+  @SuppressWarnings( "unchecked" )
   public Model read(final Reader input, final Map<String, ?> options) throws IOException, ModelParseException {
     assert manager != null;
-    ModelReader reader = manager.getReaderFor(options);
-    return reader.read(input, options);
-  }
+    ModelSource source = (ModelSource) options.get( ModelProcessor.SOURCE );
+    if ( ( "" + source ).contains(  ".tesla." ) ) {
+        System.out.println( source.getLocation() );
 
+        File pom = new File( source.getLocation() );
+        source =  new FileModelSource( new File( pom.getPath().replaceFirst( ".tesla.", "" ) ) );
+
+        ((Map)options).put( ModelProcessor.SOURCE, source );
+
+        ModelReader reader = manager.getReaderFor(options);
+        Model model = reader.read(source.getInputStream(), options);
+
+        MavenXpp3Writer xmlWriter = new MavenXpp3Writer();
+        StringWriter xml = new StringWriter();
+        xmlWriter.write( xml, model );
+
+        FileUtils.fileWrite( pom, xml.toString() );
+
+        // dump pom if filename is given via the pom properties
+        String dump = model.getProperties().getProperty( "tesla.dump.pom" );
+        if ( dump != null ) {
+            File dumpPom =  new File( pom.getParentFile(), dump );
+            if ( !dumpPom.exists() || ! FileUtils.fileRead( dumpPom ).equals( xml.toString() ) ){
+                dumpPom.setWritable( true );
+                FileUtils.fileWrite( dumpPom, xml.toString() );
+                if ( "true".equals( model.getProperties().getProperty( "tesla.dump.readonly" ) ) ){
+                    dumpPom.setReadOnly();
+                }
+            }
+        }
+
+        model.setPomFile(pom);
+        return model;
+    }
+    else {
+        ModelReader reader = manager.getReaderFor(options);
+        return reader.read(input, options);
+    }
+  }
 }
