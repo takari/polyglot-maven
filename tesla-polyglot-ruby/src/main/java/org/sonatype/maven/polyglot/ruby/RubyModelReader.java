688ca162cdbba77a2361f61072f3f1dc6d8fde0d
Christian Meier <m.kristian@web.de>
2013-07-18 20:15:57 +0100

    fix problem that the model-source from the options can be an URL as well

--- tesla-polyglot-ruby/src/main/java/org/sonatype/maven/polyglot/ruby/RubyModelReader.java
+++ tesla-polyglot-ruby/src/main/java/org/sonatype/maven/polyglot/ruby/RubyModelReader.java
@@ -19,11 +19,13 @@ package org.sonatype.maven.polyglot.ruby;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringWriter;
+import java.net.URL;
 import java.util.Map;
 
 import org.apache.maven.model.Model;
 import org.apache.maven.model.building.ModelProcessor;
 import org.apache.maven.model.building.ModelSource;
+import org.apache.maven.model.building.UrlModelSource;
 import org.apache.maven.model.io.ModelReader;
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
@@ -43,14 +45,24 @@ public class RubyModelReader extends ModelReaderSupport {
     @Requirement
     ExecuteManager executeManager = new ExecuteManagerImpl();
 
-  public Model read( final Reader input, final Map<String, ?> options ) throws IOException {
-    assert input != null;
+    public Model read( final Reader input, final Map<String, ?> options ) throws IOException {
+        assert input != null;
 
-    // read the stream from our pom.rb into a String
-    StringWriter ruby = new StringWriter();
-    IOUtil.copy( input, ruby );
-    // parse the String and create a POM model
-    return new RubyParser(( ModelSource)options.get( ModelProcessor.SOURCE ), executeManager )
-        .parse( ruby.toString() );
-  }
+        // read the stream from our pom.rb into a String
+        StringWriter ruby = new StringWriter();
+        IOUtil.copy( input, ruby );
+        // parse the String and create a POM model
+        Object src = options.get( ModelProcessor.SOURCE );
+        final ModelSource source;
+        if ( src instanceof URL )
+        {
+            source = new UrlModelSource( (URL) src );
+        }
+        else
+        {
+            source = (ModelSource) src;
+        }
+
+        return new RubyParser( source, executeManager ).parse( ruby.toString() );
+    }
 }
