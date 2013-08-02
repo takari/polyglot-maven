876b8cb44019078664bcf3e52cecfb04231e2f49
Christian Meier <m.kristian@web.de>
2013-08-02 12:25:57 +0100

    remove all pom.xml and use ONLY ruby DSLs

---
+++ tesla-polyglot-gems/README.md
@@ -0,0 +1,3 @@
+you need tesla-maven to build this module !!
+
+find the respective gems in ruby-maven/target and ruby-maven-libs/target
---
+++ tesla-polyglot-gems/pom.rb
@@ -0,0 +1,15 @@
+project 'tesla-polyglot-gems' do
+
+  model_version '4.0.0'
+  id 'io.tesla.polyglot:tesla-polyglot-gems:0.0.1-SNAPSHOT'
+  inherit 'io.tesla.polyglot:tesla-polyglot:0.0.1-SNAPSHOT'
+  packaging 'pom'
+
+  modules [ 'ruby-maven-libs',
+            'ruby-maven' ]
+
+  plugin( 'io.tesla.maven.plugins:tesla-license-plugin',
+          'header' =>  '${project.parent.basedir}/license-header.txt',
+          'excludes' => [ '**/conf/**',
+                          '**/*pom.xml' ] )
+end
