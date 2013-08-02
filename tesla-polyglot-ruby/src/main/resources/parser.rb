1efc96b8cb69d6a6135fe2f8dfc71e05f85cfa35
Christian Meier <m.kristian@web.de>
2013-08-02 12:12:25 +0100

    java model specicic stuff stays with parser.rb

--- tesla-polyglot-ruby/src/main/resources/parser.rb
+++ tesla-polyglot-ruby/src/main/resources/parser.rb
@@ -34,6 +34,8 @@ org.codehaus.plexus.util.xml.Xpp3Dom
 org.codehaus.plexus.util.xml.Xpp3DomBuilder
 ).each {|i| java_import i }
 
+Execution=PluginExecution unless defined? Execution
+
 module Tesla
   class Parser
 
@@ -54,6 +56,60 @@ module Tesla
     end
 
     include Maven::Tools::DSL
+
+    def xml( xml )
+      Xpp3DomBuilder.build( java.io.StringReader.new( xml ) )
+    end
+
+    def set_config( receiver, options )
+      if options && options.size > 0
+        config = Xpp3Dom.new("configuration")
+        fill_dom( config, options )
+        receiver.configuration = config
+      end
+    end
+
+    def fill_dom(parent, map = {})
+      map.each do |k, v|
+        case v
+        when Hash
+          child = Xpp3Dom.new(k.to_s)
+          fill_dom(child, v)
+        when Array
+          if k.to_s.match( /s$/ )
+            node = Xpp3Dom.new( k.to_s )
+            name = k.to_s.sub( /s$/, '' )
+          else
+            node = parent
+            name = k.to_s
+          end
+          v.each do |val|
+            child = Xpp3Dom.new( name )
+            case val
+            when Hash
+              fill_dom( child, val )
+              node.addChild( child )
+            when Xpp3Dom
+              node.addChild( val )
+            else
+              child.setValue( val )
+              node.addChild( child )
+            end
+          end
+          parent.addChild( node ) if node != parent
+          child = nil
+        else
+          case k.to_s
+          when /^@/
+            parent.setAttribute( k.to_s[ 1..-1 ], v.to_s )
+          else
+            child = Xpp3Dom.new(k.to_s)
+            child.setValue(v.to_s) unless v.nil?
+          end
+        end
+        parent.addChild(child) if child
+      end
+    end
   end
 end
 
