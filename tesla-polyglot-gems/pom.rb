187ddeeee144f9cc37bb4aa96836179747ccff51
Christian Meier <m.kristian@web.de>
2013-12-03 17:02:58 +0000

    use version from ruby-maven/lib/tesla_maven

--- tesla-polyglot-gems/pom.rb
+++ tesla-polyglot-gems/pom.rb
@@ -1,6 +1,11 @@
+require File.join( basedir,
+                   'ruby-maven',
+                   'ruby',
+                   'tesla_maven' )
+
 project 'tesla-polyglot-gems' do
 
-  inherit 'io.tesla.polyglot:tesla-polyglot:0.0.8'
+  inherit "io.tesla.polyglot:tesla-polyglot:#{TeslaMaven::VERSION}"
   id 'tesla-polyglot-gems'
   packaging 'pom'
 
