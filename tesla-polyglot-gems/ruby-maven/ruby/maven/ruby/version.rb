93637758d1ab87bee7e4e8b6a6d9b44de2a93031
Christian Meier <m.kristian@web.de>
2014-01-18 13:45:12 +0000

    version bump

--- tesla-polyglot-gems/ruby-maven/ruby/maven/ruby/version.rb
+++ tesla-polyglot-gems/ruby-maven/ruby/maven/ruby/version.rb
@@ -2,6 +2,6 @@ require 'maven'
 module Maven
   module Ruby
     MAVEN_VERSION = ::Maven::VERSION
-    VERSION = MAVEN_VERSION + '.0.2'# + '.dev'
+    VERSION = MAVEN_VERSION + '.0.3' + '.dev'
   end
 end
