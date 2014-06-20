c56318636fd2cfbf6b829916033a08a01cf31e29
Christian Meier <m.kristian@web.de>
2014-06-20 10:41:03 +0100

    set dependency resolution of inline execute block to compile - so the execute block can do some non-trivial stuff as well

--- tesla-polyglot-maven-plugin/src/main/java/org/sonatype/maven/polyglot/plugin/ExecuteMojo.java
+++ tesla-polyglot-maven-plugin/src/main/java/org/sonatype/maven/polyglot/plugin/ExecuteMojo.java
@@ -32,6 +32,7 @@ import org.sonatype.maven.polyglot.execute.ExecuteTask;
  * Executes registered {@link org.sonatype.maven.polyglot.execute.ExecuteTask}s.
  *
  * @goal execute
+ * @requiresDependencyResolution compile
  *
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  */
