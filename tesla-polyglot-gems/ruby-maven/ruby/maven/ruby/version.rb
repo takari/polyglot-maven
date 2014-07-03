72581b44b0c2ce5ef9636772fe65a339785c08c9
Christian Meier <m.kristian@web.de>
2014-07-03 12:34:21 +0100

    prepare release of ruby-maven gem

--- tesla-polyglot-gems/ruby-maven/ruby/maven/ruby/version.rb
+++ tesla-polyglot-gems/ruby-maven/ruby/maven/ruby/version.rb
@@ -2,6 +2,6 @@ require 'maven'
 module Maven
   module Ruby
     MAVEN_VERSION = ::Maven::VERSION
-    VERSION = MAVEN_VERSION + '.0.7'# + '.dev'
+    VERSION = MAVEN_VERSION + '.0.8'# + '.dev'
   end
 end
