06a317f4e5c2f171a5b3ece2096b65acfdd35996
gil <puntogil@libero.it>
2016-03-26 18:24:09 +0100

    add missing license headers and file

--- polyglot-clojure/src/main/clojure/org/sonatype/maven/polyglot/clojure/dsl/writer.clj
+++ polyglot-clojure/src/main/clojure/org/sonatype/maven/polyglot/clojure/dsl/writer.clj
@@ -1,3 +1,11 @@
+;;
+;; Copyright (c) 2015 to original author or authors
+;; All rights reserved. This program and the accompanying materials
+;; are made available under the terms of the Eclipse Public License v1.0
+;; which accompanies this distribution, and is available at
+;; http://www.eclipse.org/legal/epl-v10.html
+;;
+
 (ns org.sonatype.maven.polyglot.clojure.dsl.writer
  (:use org.sonatype.maven.polyglot.clojure.dsl.util)
  (:use org.sonatype.maven.polyglot.clojure.dsl.pretty-printer))
--- polyglot-clojure/src/test/resources/org/sonatype/maven/polyglot/clojure/test.leiningen.1.clj
+++ polyglot-clojure/src/test/resources/org/sonatype/maven/polyglot/clojure/test.leiningen.1.clj
@@ -1,3 +1,11 @@
+;;
+;; Copyright (c) 2015 to original author or authors
+;; All rights reserved. This program and the accompanying materials
+;; are made available under the terms of the Eclipse Public License v1.0
+;; which accompanies this distribution, and is available at
+;; http://www.eclipse.org/legal/epl-v10.html
+;;
+
 (defproject org.clojars.the-kenny/clojure-couchdb "0.2"
   :description "Simple Clojure interface to Apache CouchDB, fork of the original project with function arguments instead of *server* and some other changes."
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
