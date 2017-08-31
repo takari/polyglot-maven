b5060fd929934e19358e88bc933fef3d4bab13d1
Tobias Roeser <le.petit.fou@web.de>
2017-08-31 09:49:10 +0200

    Added test case for multi-line strings

---
+++ polyglot-scala/src/test/resources/pom-strings.scala
@@ -0,0 +1,8 @@
+import org.sonatype.maven.polyglot.scala.model._
+import scala.collection.immutable.Seq
+
+Model(
+  "io.tesla.polyglot" % "tesla-polyglot" % "0.0.1-SNAPSHOT",
+  name = """Multiline
+string"""
+)
