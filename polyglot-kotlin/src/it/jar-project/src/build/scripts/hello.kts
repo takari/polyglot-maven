3f056367ae9e68fb8e88524f72430ffbc5e540cb
Robert Thornton <ThorntonRP@ldschurch.org>
2019-03-01 20:00:07 +0000

    updated integration test

--- polyglot-kotlin/src/it/jar-project/src/build/scripts/hello.kts
+++ polyglot-kotlin/src/it/jar-project/src/build/scripts/hello.kts
@@ -1,18 +1,14 @@
-val project = bindings["project"] as org.apache.maven.project.MavenProject
-val session = bindings["session"] as org.apache.maven.execution.MavenSession
-val basedir = bindings["basedir"] as java.io.File
-val log = bindings["log"] as org.apache.maven.plugin.logging.Log
-val script = bindings["script"] as java.io.File
-val keys = bindings.keys as Collection<String>
+BuildContext(bindings).apply {
+    val keys = bindings.keys as Collection<String>
+    """
+        ------------------------------------------------------------------------
 
-"""
-    ------------------------------------------------------------------------
+        HELLO ${project.name}!!!
 
-    HELLO ${project.name}!!!
+        Folder:   ${basedir}
+        Script:   .${script.path.substringAfter(basedir.path)}
+        Bindings: ${keys}
 
-    Folder:   ${basedir}
-    Script:   .${script.path.substringAfter(basedir.path)}
-    Bindings: ${keys}
-
-    ------------------------------------------------------------------------
-""".trimIndent().lines().forEach { log.info(it) }
+        ------------------------------------------------------------------------
+    """.trimIndent().lines().forEach { log.info(it) }
+}
