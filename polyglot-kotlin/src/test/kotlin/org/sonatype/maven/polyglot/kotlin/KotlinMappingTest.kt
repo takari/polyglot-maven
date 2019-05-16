fb0b35ab6007427adfb313930347811601516a9f
Gerard de Leeuw <gdeleeuw7@gmail.com>
2019-05-16 15:10:29 +0200

    Cleanup commented code

--- polyglot-kotlin/src/test/kotlin/org/sonatype/maven/polyglot/kotlin/KotlinMappingTest.kt
+++ polyglot-kotlin/src/test/kotlin/org/sonatype/maven/polyglot/kotlin/KotlinMappingTest.kt
@@ -1,15 +1,13 @@
 package org.sonatype.maven.polyglot.kotlin
 
-import org.codehaus.plexus.PlexusTestCase
 import org.junit.Assert
 import org.junit.Test
-import org.sonatype.maven.polyglot.mapping.Mapping
 
-class KotlinMappingTests : PlexusTestCase() {
+class KotlinMappingTest {
 
     @Test
     fun testMapping() {
-        val mapping = KotlinMapping()// lookup(Mapping::class.java, "kotlin")
+        val mapping = KotlinMapping()
         val options = mapOf("org.apache.maven.model.building.source" to "/polyglot-maven/polyglot-kotlin/pom.kts")
         Assert.assertTrue(mapping.accept(options))
     }
