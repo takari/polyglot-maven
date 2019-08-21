d2e1f7c3d7787aaff93d50ed9ed09480f0a41873
Christoph Läubrich <mail@laeubi-soft.de>
2019-08-21 06:24:13 +0200

    Fix #194 ModelReaderSupport uses platformencoding

--- polyglot-common/src/main/java/org/sonatype/maven/polyglot/io/ModelReaderSupport.java
+++ polyglot-common/src/main/java/org/sonatype/maven/polyglot/io/ModelReaderSupport.java
@@ -7,21 +7,16 @@
  */
 package org.sonatype.maven.polyglot.io;
 
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.ModelParseException;
-import org.apache.maven.model.io.ModelReader;
-import org.codehaus.plexus.component.annotations.Requirement;
-import org.codehaus.plexus.logging.Logger;
-import org.codehaus.plexus.util.IOUtil;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.Map;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.ModelReader;
 
 /**
  * Support for {@link ModelReader} implementations.
@@ -31,20 +26,19 @@ import java.util.Map;
  * @since 0.7
  */
 public abstract class ModelReaderSupport implements ModelReader {
-  public Model read(final File input, final Map<String, ?> options) throws IOException {
-    Model model;
+	public Model read(final File input, final Map<String, ?> options) throws IOException {
+		try (FileInputStream inputStream = new FileInputStream(input)) {
+			Model model = read(inputStream, options);
+			model.setPomFile(input);
+			return model;
+		}
+	}
 
-    Reader reader = new BufferedReader(new FileReader(input));
-    try {
-      model = read(reader, options);
-      model.setPomFile(input);
-    } finally {
-      IOUtil.close(reader);
-    }
-    return model;
-  }
-
-  public Model read(final InputStream input, final Map<String, ?> options) throws IOException {
-    return read(new InputStreamReader(input), options);
-  }
+	public Model read(final InputStream input, final Map<String, ?> options) throws IOException {
+		return read(new InputStreamReader(input, getCharset(options)), options);
+	}
+	
+	protected Charset getCharset(Map<String, ?> options) {
+		return Charset.defaultCharset();
+	}
 }
